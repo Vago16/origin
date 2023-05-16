0a215def6b7ca856d4a14f4adfdaef8227acb185
Vago16 <116769696+Vago16@users.noreply.github.com>
2023-05-16 04:37:55 -0400

    Add files via upload

---
+++ python-ds-practice/is_palindrome.py
@@ -0,0 +1,25 @@
+def is_palindrome(phrase):
+    """Is phrase a palindrome?
+
+    Return True/False if phrase is a palindrome (same read backwards and
+    forwards).
+
+        >>> is_palindrome('tacocat')
+        True
+
+        >>> is_palindrome('noon')
+        True
+
+        >>> is_palindrome('robert')
+        False
+
+    Should ignore capitalization/spaces when deciding:
+
+        >>> is_palindrome('taco cat')
+        True
+
+        >>> is_palindrome('Noon')
+        True
+    """
+        into_one = phrase.lower().replace(' ', '')
+        return into_one == into_one[::-1]
