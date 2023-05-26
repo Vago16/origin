8ac187f707dd48330c2bcc6ebd5959b3b584cec9
Vago16 <116769696+Vago16@users.noreply.github.com>
2023-05-25 23:55:47 -0400

    Add files via upload

---
+++ Flask-Survey/app.py
@@ -0,0 +1,65 @@
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey
+
+debug = DebugToolbarExtension(app)
+
+app = Flask(__name__)
+
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+RESPONSES_KEY = "responses"
+
+@app.route("/")
+def show_survey_start():
+    """Selects a survey"""
+    return render_template("survey_start.html", survey=survey)
+
+
+@app.route("/begin", methods=["POST"])
+def start_survey():
+    """Clears the session of responses"""
+
+    return redirect("/questions/0")
+
+
+@app.route("/answer", methods=["POST"])
+def handle_question():
+    """Saves responses and redirects to the next question"""
+
+    choice = request.form['answer']
+
+    responses = session[RESPONSES_KEY]
+    responses.append(choice)
+    session[RESPONSES_KEY] = responses
+
+    if (len(responses) == len(survey.questions)):
+        return redirect("/complete")
+    else:
+        return redirect(f"/questions/{len(responses)}")
+
+
+@app.route("/questions/<int:qid>")
+def show_question(qid):
+    """Displays the current question"""
+    responses = session.get(RESPONSES_KEY)
+
+    if (responses is None):
+        return redirect("/")
+
+    if (len(responses) == len(survey.questions)):
+        return redirect("/complete")
+
+    if (len(responses) != qid):
+        flash(f"Invalid question id: {qid}.")
+        return redirect(f"/questions/{len(responses)}")
+
+    question = survey.questions[qid]
+    return render_template(
+        "question.html", question_num=qid, question=question)
+
+
+@app.route("/complete")     
+def complete():
+    """Renders survey complete page"""
+    return render_template("completion.html")
