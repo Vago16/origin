b3ff394a2267f3e8605d87cd3f9c6f6de9b5fd6c
Vago16 <116769696+Vago16@users.noreply.github.com>
2023-08-29 22:04:43 -0400

    Add files via upload

---
+++ express-jobly/helpers/helpers/sql.js
@@ -0,0 +1,20 @@
+const { BadRequestError } = require("../expressError");
+
+// makes update queries, allows the function that calls on it to use SET from a SQL statement
+
+function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+  const keys = Object.keys(dataToUpdate);
+  if (keys.length === 0) throw new BadRequestError("No data");
+
+  // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+  const cols = keys.map((colName, idx) =>
+      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+  );
+
+  return {
+    setCols: cols.join(", "),
+    values: Object.values(dataToUpdate),
+  };
+}
+
+module.exports = { sqlForPartialUpdate };
