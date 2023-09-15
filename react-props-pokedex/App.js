644e2dd38f2dfcb9f5bb91773f24b3205fdd9930
Vago16 <116769696+Vago16@users.noreply.github.com>
2023-09-14 20:21:49 -0400

    Add files via upload

---
+++ react-props-pokedex/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Pokedex from "./Pokedex";
+
+/** Main App component. */
+
+function App() {
+  return (
+      <div className="App">
+        <Pokedex />
+      </div>
+  );
+}
+
+export default App;
